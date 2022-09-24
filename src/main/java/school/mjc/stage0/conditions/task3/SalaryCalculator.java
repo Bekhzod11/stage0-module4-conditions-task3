7f64f75287aa12109028d295198cabed25b22f5a
Bekhzod <Bekhzod1411@gmail.com>
2022-09-24 23:39:48 +0500

    new
    new

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary<=1000){
+            System.out.println(salary-salary*0.15);
+        } else if (salary>1000 && salary<=2000) {
+            System.out.println(salary-salary*0.18);
+        } else if (salary>2000){
+            System.out.println(salary-salary*0.20);
+        } else System.out.println("wrong input");
     }
 }
